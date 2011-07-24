0a47d45b68a242e4763eb8d93ebc6bb45475122f
unknown <Administrador@.(none)>
2011-07-23 22:34:34 -0300

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def logo
+    image_tag("http://railstutorial.org/images/sample_app/logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+  def title 
+    base_title = "Ruby on Rails Tutorial Sample Twitter"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -38,5 +38,17 @@ describe PagesController do
       response.should have_selector("title", :content => 'Ruby on Rails Tutorial Sample Twitter | About')
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => 'Ruby on Rails Tutorial Sample Twitter | Help')
+    end
+  end
 
 end
