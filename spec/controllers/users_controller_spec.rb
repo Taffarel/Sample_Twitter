e06d49e40d7677c0bcc7cdf888852170fde8e9df
Taffarel de Lima <taffarelo3@hotmail.com>
2011-07-25 22:56:07 -0300

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -42,6 +42,11 @@ describe UsersController do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
+      
+       it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
 
       it "should redirect to the user show page" do
         post :create, :user => @attr
@@ -50,7 +55,7 @@ describe UsersController do
       
       it "should have a welcome message" do
         post :create, :user => @attr
-        flash[:success].should =~ /welcome to the sample app/i
+        flash[:success].should =~ /welcome to the sample Twitter/i
       end
     end
   end
